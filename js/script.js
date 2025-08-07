e9ffe79597cbf3ae63991c05a0ad4bb3478d0827
Hisham Khartoum <hishamkhartoum@gmail.com>
2025-08-07 13:16:54 +0100

    Updated code: removed old files, added new structure

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+const toggle = document.querySelector('.menu-toggle');
+const header = document.querySelector('.header');
+
+toggle.addEventListener('click', () => {
+  header.classList.toggle('nav-open');
+});
